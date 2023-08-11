cc62a4194c8310e28ae4bd17018e78e345bbf787
Test Axel <testaxel@mbpmm0042.local>
2023-08-11 13:03:29 +0200

    Add React

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./styles/main.scss";
-console.log("okoko");
-console.log(1);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const el = document.getElementById("root");
+
+ReactDOM.render(<App />, el);
